e55332d3a0db45245898ce35e0f1e9fae6471860
Adam Duff <j.adam.duff@gmail.com>
2018-11-08 19:07:17 +0000

    automatically backed up by learn

---
+++ app/controllers/repositories_controller.rb
@@ -0,0 +1,11 @@
+class RepositoriesController < ApplicationController
+
+  def index
+    resp = Faraday.get("https://api.github.com/user") do |req|
+      req.headers["Authorization"] = 'token ' + session[:token]
+    end
+
+    binding.pry
+  end
+
+end
